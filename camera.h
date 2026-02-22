298a227a030351aae9816ac5890c842e4bb49dd8
NingQung <yorha21s@gmail.com>
2026-02-22 21:06:26 +0800

    2ch7 fin

--- camera.h
+++ camera.h
@@ -10,6 +10,7 @@ class camera {
     int    image_width  = 100;  // Rendered image width in pixel count
     int    samples_per_pixel = 5;   // Count of random samples for each pixel
     int    max_depth         = 10;   // Maximum number of ray bounces into scene
+    color  background;               // Scene background color
 
     double vfov = 90;  // Vertical view angle (field of view)
     point3 lookfrom = point3(0,0,0);   // Point camera is looking from
@@ -121,6 +122,21 @@ class camera {
             return color(0,0,0);
 
         hit_record rec;
+
+        // If the ray hits nothing, return the background color.
+        if (!world.hit(r, interval(0.001, infinity), rec))
+            return background;
+
+        ray scattered;
+        color attenuation;
+        color color_from_emission = rec.mat->emitted(rec.u, rec.v, rec.p);
+
+        if (!rec.mat->scatter(r, rec, attenuation, scattered))
+            return color_from_emission;
+
+        color color_from_scatter = attenuation * ray_color(scattered, depth-1, world);
+
+        return color_from_emission + color_from_scatter;
 
         if (world.hit(r, interval(0.001, infinity), rec)) {
             ray scattered;
