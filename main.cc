b67cb698e1fc2897728b40b9965f426077a5b8d3
NingQung <yorha21s@gmail.com>
2026-05-16 21:13:27 +0800

    adjust: scene details
    看起來是有了，需要完整跑吧

--- main.cc
+++ main.cc
@@ -25,7 +25,7 @@ int main() {
     auto blue = make_shared<lambertian>(color(.05, .05, .65));
     auto yellow = make_shared<lambertian>(color(.70, .70, .05));
     auto green = make_shared<lambertian>(color(.12, .45, .15));
-    auto light = make_shared<diffuse_light>(color(20.0, 20.0, 20.0));
+    auto light = make_shared<diffuse_light>(color(40.0, 40.0, 40.0));
     shared_ptr<material> aluminum = make_shared<metal>(color(0.8, 0.85, 0.88), 0.0);
     auto glass = make_shared<dielectric>(1.7, 0.015);
     auto checker_tex = make_shared<checker_texture>(40.0,color(.85, .85, .85),color(.10, .10, .10));
@@ -78,6 +78,7 @@ int main() {
 
       // prism
       shared_ptr<hittable> prism1 = prism(point3(0,0,0), point3(-150,0,180), point3(150,0,180), 400.0, glass);
+      prism1 = make_shared<rotate_x>(prism1, 15);
       prism1 = make_shared<translate>(prism1, vec3(275,5,75));
       world.add(prism1);
 
@@ -88,19 +89,21 @@ int main() {
       break;
     }
     case 3: {
+      auto glass2 = make_shared<dielectric>(1.5, 0.015);
       world.add(make_shared<quad>(point3(555,0,0), vec3(0,0,555), vec3(0,555,0), green));
       world.add(make_shared<quad>(point3(0,0,555), vec3(0,0,-555), vec3(0,555,0), red));
       world.add(make_shared<quad>(point3(0,555,0), vec3(555,0,0), vec3(0,0,555), white)); //up
       world.add(make_shared<quad>(point3(0,0,555), vec3(555,0,0), vec3(0,0,-555), white)); //buttom
       world.add(make_shared<quad>(point3(555,0,555), vec3(-555,0,0), vec3(0,555,0), wave_mat)); //back
-      world.add(make_shared<quad>(point3(0,300,270), vec3(555,0,0), vec3(0,0,-275), dark)); //front-blocker
-      world.add(make_shared<quad>(point3(0,300,555), vec3(555,0,0), vec3(0,0,-280), dark)); //back-blocker
+      world.add(make_shared<quad>(point3(0,300,270), vec3(555,0,0), vec3(0,0,-275), white)); //front-blocker
+      world.add(make_shared<quad>(point3(0,300,556), vec3(555,0,0), vec3(0,0,-280), white)); //back-blocker
 
-      world.add(make_shared<quad>(point3(0,554,280), vec3(0,0,-10), vec3(555,0,0), light));
-      lights.add(make_shared<quad>(point3(0,554,280), vec3(0,0,-10), vec3(555,0,0), light));
+      world.add(make_shared<quad>(point3(0,554,280), vec3(0,0,-30), vec3(555,0,0), light));
+      lights.add(make_shared<quad>(point3(0,554,280), vec3(0,0,-30), vec3(555,0,0), light));
 
-      shared_ptr<hittable> prism1 = prism(point3(0,0,0), point3(0,50,60), point3(0,-50,60), 400.0, glass);
-      prism1 = make_shared<translate>(prism1, vec3(75,80,230));
+      shared_ptr<hittable> prism1 = prism(point3(0,0,0), point3(0,65,100), point3(0,-65,100), 400.0, glass2);
+      prism1 = make_shared<rotate_x>(prism1, -60);
+      prism1 = make_shared<translate>(prism1, vec3(75,25,250));
       world.add(prism1);
 
       cam.vfov     = 40;
